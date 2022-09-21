cc5eeaf60e7c8db64cb97a4d38b9404618fe4cd1
abi <abimael.monarrez58@gmail.com>
2022-09-21 13:48:35 -0600

    create entity models

---
+++ src/main/java/com/technews/VariablePractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablePractice {
+    public static void main(String[] args) {
+        int integer = 1;
+        double decimal = 10.5;
+        double sum = integer + decimal;
+
+        String greeting = "Hello";
+        String alphanumeric = greeting + integer;
+        System.out.println(sum);
+        System.out.println(alphanumeric);
+    }
+}
